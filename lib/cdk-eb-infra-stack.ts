a775e5ea5a984924bbac3b277de85d20d22619c9
josh gulledge <joshthedevgulledge@proton.me>
2022-09-12 22:13:17 -0500

    initial commit

--- lib/cdk-eb-infra-stack.ts
+++ lib/cdk-eb-infra-stack.ts
@@ -1,5 +1,8 @@
 import * as cdk from 'aws-cdk-lib';
 import { Construct } from 'constructs';
+import s3assets = require('@aws-cdk/aws-s3-assets');
+import elasticbeanstalk = require('@aws-cdk/aws-elasticbeanstalk');
+
 // import * as sqs from 'aws-cdk-lib/aws-sqs';
 
 export class CdkEbInfraStack extends cdk.Stack {
@@ -7,6 +10,27 @@ export class CdkEbInfraStack extends cdk.Stack {
     super(scope, id, props);
 
     // The code that defines your stack goes here
+    const webAppZipArchive = new s3assets.Asset(this, 'WebAppZip', {
+      path: `${__dirname}/../app.zip`,
+    });
+
+    // Create a ElasticBeanStalk app.
+    const appName = 'MyWebApp';
+    const app = new elasticbeanstalk.CfnApplication(this, 'Application', {
+      applicationName: appName,
+    });
+
+    // Create an app version from the S3 asset defined earlier
+    const appVersionProps = new elasticbeanstalk.CfnApplicationVersion(this, 'AppVersion', {
+      applicationName: appName,
+      sourceBundle: {
+        s3Bucket: webAppZipArchive.s3BucketName,
+        s3Key: webAppZipArchive.s3ObjectKey,
+      },
+    });
+
+    // Make sure that Elastic Beanstalk app exists before creating an app version
+    appVersionProps.addDependsOn(app);
 
     // example resource
     // const queue = new sqs.Queue(this, 'CdkEbInfraQueue', {
